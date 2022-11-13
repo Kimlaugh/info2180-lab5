18ab292ddfe7e38af8942d437b330fe63123003d
Kimberly Laughton <kimberlyclaughton@gmail.com>
2022-11-13 17:39:39 -0500

    completed exercise 4

--- world.php
+++ world.php
@@ -14,13 +14,24 @@ $countryquery = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country
 $results = $countryquery->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-
 <?php
 header('Access-Control-Allow-Origin: *');
 ?>
-
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<?php
+echo("<table>");
+    echo("<tr>");
+      echo("<th>Country Name</th>");
+      echo("<th>Continent</th>");
+      echo("<th>Independence Year</th>");
+      echo("<th>Head of State</th>");
+    echo("</tr>");
+  foreach ($results as $row){
+    echo("<tr>");
+      echo("<td>".$row["name"]."</td>");
+      echo("<td>".$row["continent"]."</td>");
+      echo("<td>".$row["independence_year"]."</td>");
+      echo("<td>".$row["head_of_state"]."</td>");
+    echo("</tr>");
+  }
+echo("</table>");
+?>
